7a792639b193cbb1127c68bda4df18389c06a9da
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2022-12-13 22:51:19 +0100

    Remove ADC and SPI

--- Inc/adc.h
+++
@@ -1,9 +0,0 @@
-
-
-#ifndef ADC_H_
-#define ADC_H_
-
-void ADC_Init(void);
-void ADC_Conv(void);
-
-#endif
--- Inc/spi.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPI_H_
-#define SPI_H_
-
-void SPI2_Init(void);
-
-#endif /* SPI_H_ */
