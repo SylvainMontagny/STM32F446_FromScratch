80a0bc722aa44905a148abe4e7f2237b675de294
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2024-09-09 13:48:46 +0200

    Add pin managment functions : Set-Reset-Toggle-Read

--- Inc/gpio.h
+++ Inc/gpio.h
@@ -2,8 +2,10 @@
 #define GPIO_H_
 
 void GPIO_Init(void);
-void GPIOA_Set(void);
-void GPIOA_Reset(void);
-void GPIOA_Toggle(void);
+
+void 		hal_SetPin(GPIO_TypeDef* GPIO, uint16_t pin);
+void 		hal_ResetPin(GPIO_TypeDef* GPIO, uint16_t pin);
+void 		hal_TogglePin(GPIO_TypeDef* GPIO, uint16_t pin);
+uint16_t 	hal_ReadPin(GPIO_TypeDef* GPIO, uint16_t pin);
 
 #endif
--- Src/gpio.c
+++ Src/gpio.c
@@ -12,3 +12,20 @@ void GPIO_Init(void){
 	GPIOC->MODER |= INPUT_MODE	<< 26;		// PC13 Input Push-Button
 }
 
+
+void GPIO_SetPin(GPIO_TypeDef* GPIO, uint16_t pin){
+	GPIO->ODR |= 1 << pin ;
+}
+
+void GPIO_ResetPin(GPIO_TypeDef* GPIO, uint16_t pin){
+	GPIO->ODR &= ~(1 << pin) ;
+}
+
+void GPIO_TogglePin(GPIO_TypeDef* GPIO, uint16_t pin){
+	GPIO->ODR ^= 1 << pin ;
+}
+
+uint16_t GPIO_ReadPin(GPIO_TypeDef* GPIO, uint16_t pin){
+	if((GPIO->IDR & (1 << pin)) !=0)	return 1;
+	else 								return 0;
+}
